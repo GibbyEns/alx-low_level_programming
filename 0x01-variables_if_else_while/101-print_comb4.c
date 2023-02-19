b52bb0a33c553629594058175c41b3b46c1f2f5b
Enoch <gibbymerc@gmail.com>
2023-02-19 12:33:31 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -16,4 +17,19 @@ int main(void)
 			{
 				if (l > m && m > n)
 				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
 
